dffba5c59269081f41c63053b03229d1d6486e57
chendumpling99 <66714443+chendumpling99@users.noreply.github.com>
2022-08-01 18:42:47 -0700

    set up tailwind css with react typescript

---
+++ frontend/src/App.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { StyledForm } from "./styles";
+
+function App() {
+  return (
+    <h1>Hello World!</h1>
+    // <StyledForm>
+    //   <form>
+    //     <input type="text" placeholder="Full name" />
+    //     <input type="text" placeholder="Email" />
+    //     <input type="text" placeholder="Password" />
+    //     <button>Sign In</button>
+    //   </form>
+    // </StyledForm>
+  );
+}
+
+export default App;
